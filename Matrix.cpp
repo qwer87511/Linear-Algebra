be75a158e5163205aaa4bc1f1d34d28309799d41
Joe <35094020+qwer87511@users.noreply.github.com>
2018-01-30 23:18:05 +0800

    Matrix using template

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,192 +1,46 @@
-#include "Matrix.h"
-#include <iostream>
-#include <iomanip>
-#include <string>
+#include<iostream>
 using namespace std;
 
-Matrix::Matrix(const int& column, const int& row) : column(column), row(row)
+template<class T>
+class Matrix
 {
-    this->Malloc(column, row);
-    this->SetDiagonalMatrix(0);
-}
-Matrix::Matrix(const Matrix& rhs) : column(rhs.column), row(rhs.row)
+    public:
+        Matrix(const int&, const int&);
+
+        /* operator overloading */
+
+        // I / O
+
+        friend istream& operator >> (istream&, Matrix&);
+        friend ostream& operator << (ostream&, const Matrix&);
+
+        /* properties */
+        int column;
+        int row;
+        T** data;
+};
+
+// constructor
+template<class T>
+Matrix<T>::Matrix(const int& column, const int& row) : column(column), row(row)
 {
-    this->Malloc(column, row);
-    this->Copy(rhs);
-}
-Matrix::~Matrix()
-{
-    this->Clear();
-}
-void Matrix::Malloc(int column, int row)
-{
-    this->data = new Fraction*[column];
+    this->data = new T*[column];
 
     for (int i = 0; i < column; ++i)
     {
-        this->data[i] = new Fraction[row];
+        this->data[i] = new T[row];
     }
 }
-void Matrix::Copy(const Matrix& rhs)
-{
-    for (int i = 0; i < column; ++i)
-    {
-        for (int j = 0; j < row; ++j)
-        {
-            this->data[i][j] = rhs.data[i][j];
-        }
-    }
-}
-void Matrix::Reset(int column, int row)
-{
-    this->Clear();
-    this->column = column;
-    this->row = row;
-    this->Malloc(column, row);
-    this->SetDiagonalMatrix(0);
-}
-void Matrix::SetDiagonalMatrix(const int& num)
-{
-    for (int i = 0; i < column; ++i)
-    {
-        for (int j = 0; j < row; ++j)
-        {
-            if (i == j) this->data[i][j] = num;
-            else this->data[i][j] = 0;
-        }
-    }
-}
-void Matrix::Clear()
-{
-    for (int i = 0; i < column; ++i)
-    {
-        delete[] data[i];
-    }
 
-    delete[] data;
-}
-Matrix& Matrix::operator=(const Matrix& rhs)
+// I/O
+template<class T>
+istream& operator >> (istream& in, Matrix<T>& m)
 {
-    if (this->column != rhs.column || this->row != rhs.row)
-        this->Reset(column, row);
+    return in >> m.data[0][0];
+}
 
-    this->Copy(rhs);
-    return *this;
-}
-istream& operator >> (istream& in, Matrix& m)
+template<class T>
+ostream& operator<<(ostream& out, const Matrix<T>& m)
 {
-    for (int i = 0; i < m.column; ++i)
-    {
-        for (int j = 0; j < m.row; ++j)
-        {
-            cin >> m.data[i][j];
-        }
-    }
-
-    return in;
-}
-ostream& operator << (ostream& out, const Matrix& m)
-{
-    for (int i = 0; i < m.column; ++i)
-    {
-        cout << '|';
-
-        for (int j = 0; j < m.row; ++j)
-        {
-            cout << setw(3) << m.data[i][j];
-        }
-
-        cout << setw(3) << '|' << endl;
-    }
-
-    return out;
-}
-Matrix Matrix::operator-() const
-{
-    return Matrix(*this) *= -1;
-}
-Matrix& operator+=(Matrix& lhs, const Matrix& rhs)
-{
-    if (lhs.column != rhs.column || lhs.row != rhs.row) throw Matrix::DIFFERENT_SIZE;
-
-    for (int i = 0; i < lhs.column; ++i)
-    {
-        for (int j = 0; j < lhs.row; ++j)
-        {
-            lhs.data[i][j] = lhs.data[i][j] + rhs.data[i][j];
-        }
-    }
-
-    return lhs;
-}
-Matrix operator+(const Matrix& lhs, const Matrix& rhs)
-{
-    return Matrix(lhs) += rhs;
-}
-Matrix& operator-=(Matrix& lhs, const Matrix& rhs)
-{
-    return lhs += -rhs;
-}
-Matrix operator-(const Matrix& lhs, const Matrix& rhs)
-{
-    return Matrix(lhs) += -rhs;
-}
-Matrix& operator*=(Matrix& lhs, const Matrix& rhs)
-{
-    if (lhs.row != rhs.column) throw Matrix::MULTIPLE_ERRPOR;
-
-    Matrix lhsTemp(lhs);
-    lhs.Reset(lhs.column, rhs.row);
-    cout << lhs << endl << rhs << endl;
-    /*for (int i = 0; i < lhsTemp.column; ++i)
-    {
-        for (int j = 0; j < rhs.row; ++j)
-        {
-            for (int k = 0; k < rhs.column; ++k)
-            {
-                cout << "data[" << i << "][" << j << "] += data[" << i << "][" << k << "] * data[" << k << "][" << j << "]" << endl;
-                cout << lhs.data[i][j] << " += " << lhsTemp.data[i][k] << " * " << rhs.data[k][j] << endl;
-                lhs.data[i][j] += lhsTemp.data[i][k] * rhs.data[k][j];
-            }
-        }
-    }*/
-    return lhs;
-}
-Matrix& operator*=(Matrix& m, const int& mul)
-{
-    for (int i = 0; i < m.column; ++i)
-    {
-        for (int j = 0; j < m.row; ++j)
-        {
-            m.data[i][j] *= mul;
-        }
-    }
-
-    return m;
-}
-Matrix operator*(const Matrix& lhs, const Matrix& rhs)
-{
-    return Matrix(lhs) *= rhs;
-}
-Matrix operator*(const Matrix& m, const int& mul)
-{
-    return Matrix(m) *= mul;
-}
-Matrix operator*(const int& mul, const Matrix& m)
-{
-    return Matrix(m) *= mul;
-}
-Matrix Matrix::Inverse()
-{
-    Matrix result(this->column, this->row);
-
-    for (int i = 0; i < column; ++i)
-    {
-        for (int j = 0; j < row; ++j)
-        {
-            result.data[i][j] = 1 / this->data[i][j];
-        }
-    }
-
-    return result;
+    return out << m.data[0][0];
 }
--- Matrix.h
+++ Matrix.h
@@ -1,46 +1,256 @@
 #pragma once
-#include "Fraction.h"
+#include<iostream>
+#include<iomanip>
+using namespace std;
 
+template<class T>
 class Matrix
 {
     public:
+
+        // constructor
         Matrix(const int& = 1, const int& = 1);
+        Matrix(const Matrix&);
+        // destructor
         ~Matrix();
 
-        enum ERROR_CODE { MULTIPLE_ERRPOR, DIFFERENT_SIZE };
+        //-------------------------------------------------------------------------
 
-        Matrix(const Matrix&);
-        void Malloc(int, int);
-        void Reset(int, int);
-        void Clear();
-        void Copy(const Matrix&);
-        void SetDiagonalMatrix(const int&);
+        // to construct
+        Matrix& Copy(const Matrix&);
+        Matrix& Swap(Matrix&); // 減少使用new
 
-        /* operator overloading */
+        // to destruct
+        //void Clear();
+        //void Reset(const int&, const int&);
 
+        //-------------------------------------------------------------------------
+
+        // operator overloading
+
+        // index operator
+        T* operator[](int);
+
+        // assign
         Matrix& operator=(const Matrix&);
 
-        // I/O
+        // Input
+        template<class T> friend istream& operator >> (istream&, Matrix&);
+        template<class T> friend ostream& operator << (ostream&, const Matrix&);
 
-        friend istream& operator >> (istream&, Matrix&);
-        friend ostream& operator << (ostream&, const Matrix&);
-
-        Matrix Inverse();
-
-        Matrix operator-() const;
-
-        friend Matrix& operator+=(Matrix&		, const Matrix&);
-        friend Matrix  operator+ (const Matrix&	, const Matrix&);
-        friend Matrix& operator-=(Matrix&		, const Matrix&);
-        friend Matrix  operator- (const Matrix&	, const Matrix&);
-        friend Matrix& operator*=(Matrix&		, const Matrix&);
-        friend Matrix& operator*=(Matrix&		, const int&);
-        friend Matrix  operator* (const Matrix&	, const Matrix&);
-        friend Matrix  operator* (const Matrix&	, const int&);
-        friend Matrix  operator* (const int&	, const Matrix&);
+        // operation
+        template<class T> friend Matrix& operator+=(Matrix&			, const Matrix&);
+        template<class T> friend Matrix	 operator+ (const Matrix&	, const Matrix&);
+        template<class T> friend Matrix& operator-=(Matrix&			, const Matrix&);
+        template<class T> friend Matrix	 operator- (const Matrix&	, const Matrix&);
+        template<class T> friend Matrix& operator*=(Matrix&			, const Matrix&);
 
         /* properties */
         int column;
         int row;
-        Fraction** data;
+        T** data;
 };
+
+// constructor
+template<class T>
+Matrix<T>::Matrix(const int& column, const int& row) :
+    column(column),
+    row(row),
+    data(new T*[column])
+{
+    for (int i = 0; i < column; ++i)
+    {
+        this->data[i] = new T[row] { 0 }; // T 需要支援初始參數.
+    }
+}
+
+// copy constructor
+template<class T>
+Matrix<T>::Matrix(const Matrix<T>& m) :
+    column(m.column),
+    row(m.row),
+    data(new T*[column])
+{
+    for (int i = 0; i < column; ++i)
+    {
+        this->data[i] = new T[row] { 0 }; // T 需要支援初始參數.
+    }
+
+    this->Copy(m);
+}
+
+// destructor
+template<class T>
+Matrix<T>::~Matrix()
+{
+    for (int i = 0; i < column; ++i)
+    {
+        delete[] this->data[i];
+    }
+
+    delete[] this->data;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::Copy(const Matrix<T>& m)
+{
+    for (int i = 0; i < column; ++i)
+    {
+        memcpy(this->data[i], m.data[i], this->row * sizeof(T));
+    }
+
+    return *this;
+}
+
+//template<class T>
+//void Matrix<T>::Clear()
+//{
+//    this->Swap(Matrix<T>());
+//}
+
+//template<class T>
+//void Matrix<T>::Reset(const int& column, const int& row)
+//{
+//    this->Clear();
+//    this->column = column;
+//    this->row = row;
+//    this->Malloc(column, row);
+//}
+
+template<class T>
+Matrix<T>& Matrix<T>::Swap(Matrix<T>& rhs)
+{
+    swap(this->column, rhs.column);
+    swap(this->row, rhs.row);
+    swap(this->data, rhs.data);
+    return *this;
+}
+
+// operator overloading
+template<class T>
+Matrix<T>& Matrix<T>::operator=(const Matrix<T>& m)
+{
+    if (this->column != m.column || this->row != m.row)
+        //this->Reset(m.column, m.row); // version 1
+        return this->Swap(Matrix<T>(m)); // version 2
+    else
+        return this->Copy(m);
+}
+
+// input
+template<class T>
+istream& operator >> (istream& in, Matrix<T>& m)
+{
+    for (int i = 0; i < m.column; ++i)
+    {
+        for (int j = 0; j < m.row; ++j)
+        {
+            in >> m.data[i][j];
+        }
+    }
+
+    return in;
+}
+
+// output
+template<class T>
+ostream& operator << (ostream& out, const Matrix<T>& m)
+{
+    for (int i = 0; i < m.column; ++i)
+    {
+        out << setw(3) << "|";
+
+        for (int j = 0; j < m.row; ++j)
+        {
+            out <<setw(3)<< m.data[i][j];
+        }
+
+        out << setw(3) << "|" << endl;
+    }
+
+    return out;
+}
+
+// index operation
+template<class T>
+T* Matrix<T>::operator[](int index)
+{
+    return this->data[index];
+}
+
+// operation
+
+// Matrix += Matrix
+template<class T>
+Matrix<T>& operator+=(Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+    for (int i = 0; i < rhs.column; ++i)
+    {
+        for (int j = 0; j < rhs.row; ++j)
+        {
+            lhs.data[i][j] += rhs.data[i][j];
+        }
+    }
+
+    return lhs;
+}
+
+// Matrix + Matrix
+template<class T>
+Matrix<T> operator+(const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+    return Matrix<T>(lhs) += rhs;
+}
+
+// Matrix -= Matrix
+template<class T>
+Matrix<T>& operator-=(Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+    for (int i = 0; i < rhs.column; ++i)
+    {
+        for (int j = 0; j < rhs.row; ++j)
+        {
+            lhs.data[i][j] -= rhs.data[i][j];
+        }
+    }
+
+    return lhs;
+}
+
+// Matrix - Matrix
+template<class T>
+Matrix<T> operator-(const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+    return Matrix<T>(lhs) -= rhs;
+}
+
+// Matrix *= Matrix
+template<class T>
+Matrix<T>& operator*=(Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+    if (lhs.row != rhs.column) throw "MULTIPLE ERROR";
+
+    // if rhs matrix is not square lhs matrix would change the size
+    Matrix<T> result(lhs.column, rhs.row);
+
+    for (int i = 0; i < lhs.column; ++i)
+    {
+        for (int j = 0; j < rhs.row; ++j)
+        {
+            for (int k = 0; k < lhs.row; ++k)
+            {
+                result.data[i][j] += lhs.data[i][k] * rhs.data[k][j];
+            }
+        }
+    }
+
+    lhs.Swap(result);
+    return lhs;
+}
+
+// Matrix * Matrix
+template<class T>
+Matrix<T> operator*(const Matrix<T>& lhs, const Matrix<T>& rhs)
+{
+    return Matrix<T>(lhs) *= rhs;
+}
